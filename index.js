3c4bef1940965cf4180880033580587013d837e5
fernandolessaksouza-wq <fernando.lessak.souza@escola.pr.gov.br>
2026-05-21 11:16:34 +0000

    Node.js criando sua primeira biblioteca - Carregamento de arquivos

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+import fs from 'fs';
 import chalk from 'chalk';
+  
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura [1]]: captura[2]}))
+    return resultados;
+}
 
-console.log(chalk.red('quero'), chalk.blue('dormir') );
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+     const encoding = 'utf-8';
+     const texto = await fs.promises.readFile(caminhoDoArquivo,encoding)
+     console.log(extraiLinks(texto));
+    }  catch (erro) {
+    trataErro(erro)
+    }
+    
+}
+
+// promises com then()
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.blue(texto)))
+//      .catch(trataErro)
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { 
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//     console.log(chalk.red(texto));
+//    })
+//}
+
+pegaArquivo('./arquivos/texto.md');
+
+
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+
+//console.log(chalk.black('preguiça'));
